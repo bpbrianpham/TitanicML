ff1111c1fa9ca478bca5d14feb405a2018f04b45
bpham <brianthongpham@gmail.com>
2018-04-05 23:14:26 -0700

    helper update

--- Code/main.py
+++ Code/main.py
@@ -1,32 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr  3 17:42:13 2018
-
-@author: Brian
-"""
-
 import csv
 import tensorflow as tf
 import numpy as np
 import pandas as pd
 
-'''
-with open("../Data/train.csv", newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in reader:
-        print(' '.join(row))
-        
-'''        
-df = pd.read_csv("../Data/train.csv")
+       
+df = pd.read_csv("../Data/train.csv", usecols=(0,1,2,4,5,6,7,9))
 print(df)
-'''
-#data = np.loadtxt(open("../Data/train.csv", "rb"), delimiter=",", skiprows=1)
+print(type(df))
+Titanic_Training_Data = df.as_matrix()
+print(Titanic_Training_Data)
 
-def load_data(dataloc):
-	data = np.loadtxt(dataloc, unpack='true')
-	#data = np.transpose(data, (1,0))
-	return data	
-
-data = load_data("../Data/train.csv")
-#print(data)
-'''

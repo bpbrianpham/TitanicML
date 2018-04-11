5da92b89ddb57bc3291dc86de759ea725be59182
bpham <brianthongpham@gmail.com>
2018-04-10 17:21:35 -0700

    what am i commiting

--- Code/helper.py
+++ Code/helper.py
@@ -3,10 +3,13 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 
+'''
+"../Data/train.csv"
+'''
 
 def load_features(d):
-    df = pd.read_csv(d, usecols=(0,2,4,5,6,7,9))
-    df2 = pd.read_csv(d, usecols=(0,1))
+    df = pd.read_csv(d, usecols=(2,4,5,6,7,9))
+    df2 = pd.read_csv(d, usecols=(1))
     data = df.as_matrix()
     label = df2.as_matrix()
     return np.matrix(data), np.matrix(label)

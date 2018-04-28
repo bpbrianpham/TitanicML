32cb5dda96aed39737e446ee1100eb7c8af0393f
DirtbagDrew <andrewmyerhb@gmail.com>
2018-04-28 16:29:00 -0700

    added cabin letter

--- Code/logistic_regression_1.py
+++ Code/logistic_regression_1.py
@@ -29,18 +29,46 @@ def load_data(filepath):
     df.replace("female", 0, inplace=True)
     
     df["Age"] = df[["Age", "Pclass"]].apply(impute_age, axis=1)
-    #df["Age"] = normalize(df["Age"])
+    df["Age"] = normalize(df["Age"])
     #df["Fare"] = normalize(df["Fare"])
-    #df["SibSp"] = normalize(df["SibSp"])
-    
+    df["SibSp"] = normalize(df["SibSp"])
+    df['Cabin_letter']=df['Cabin'].str[:1]
+    df['Cabin_letter A'] = np.where(df['Cabin_letter']=='A',1,0)
+    df['Cabin_letter B'] = np.where(df['Cabin_letter']=='B',1,0)
+    df['Cabin_letter C'] = np.where(df['Cabin_letter']=='C',1,0)
+    df['Cabin_letter D'] = np.where(df['Cabin_letter']=='D',1,0)
+    df['Cabin_letter E'] = np.where(df['Cabin_letter']=='E',1,0)
+    df['Cabin_letter noCabin'] = np.where(df['Cabin_letter'].isnull(),1,0)
+    #Cabin = pd.get_dummies(df["Cabin"], drop_first=True)
+    #Cabin_letter = pd.get_dummies(df["Cabin_letter"], drop_first=True)
+    #pdb.set_trace()
     
     #turn embarked into 0s and 1s
-    embark = pd.get_dummies(df["Embarked"], drop_first=True)
-    embark.head()
-    df = pd.concat([df, embark], axis = 1)
+    embark = pd.get_dummies(df['Embarked'],prefix='Embarked ',drop_first=True)
+    #cabin = pd.get_dummies(df['Cabin'],prefix='cabin ',drop_first=True)
+    #cabin_letter= pd.get_dummies(df['Cabin_letter'],prefix='cabin_letter ',drop_first=True)
+    
     df.drop(["Embarked"], axis = 1, inplace = True)
+    df.drop(["Cabin"], axis = 1, inplace = True)
+    df.drop(["Cabin_letter"], axis = 1, inplace = True)
+    #pdb.set_trace()
+    datas=[df,embark]
+    df=pd.concat(datas, axis=1)
+    #embark.head()
+    #df = embark
+    
+    #df.head()
+    
+    '''Cabin.head()
+    df = pd.concat([df, Cabin], axis = 1)
+    df.drop(["Cabin"], axis = 1, inplace = True)
     df.head()
     
+    Cabin_letter.head()
+    df = pd.concat([df, Cabin_letter], axis = 1)
+    df.head()
+    '''
+   # df = df[~df.index.duplicated()]
     return df
 
 def normalize(series):
@@ -62,13 +90,14 @@ def impute_age(cols):
 
 
 if __name__ == '__main__':
-    
+    #pdb.set_trace()
     df = load_data("../Data/train.csv")
     
-#    df.drop(["Name", "PassengerId"], axis = 1, inplace=True)
     
-    trainData = df.as_matrix(columns=["Pclass", "Sex", "Age", "SibSp", "Parch", "Q", "S"] )
+    
     trainLabel = df.as_matrix(columns=["Survived"]).astype(float)
+    df.drop(["Name", "PassengerId","Fare","Ticket","Survived"], axis = 1, inplace=True)
+    trainData = df.as_matrix()
     '''
     logreg=LogisticRegression()
     logreg.fit(trainData,trainLabel)
@@ -88,12 +117,12 @@ if __name__ == '__main__':
     '''
     
     
-    #pdb.set_trace()
+    
     model = Sequential()
     #model.add(Dense(1, input_shape=(8,)))
     #model.add(Dropout(0.25))
     #model.add(Dense(2))
-    model.add(Dense(2, activation="softmax", input_shape=(7,)))
+    model.add(Dense(2, activation="softmax", input_shape=(13,)))
     #model.summary()
     #opt = SGD()
     opt = Adam()
@@ -113,7 +142,8 @@ if __name__ == '__main__':
     plt.show()
     '''
     df2 = load_data("../Data/test.csv")
-    testData = df2.as_matrix(columns=["Pclass", "Sex", "Age", "SibSp", "Parch", "Q", "S"] )
+    df2.drop(["Name", "PassengerId","Fare","Ticket"], axis = 1, inplace=True)
+    testData = df2.as_matrix()
     
     df3 = pd.read_csv("../Data/gender_submission.csv")
     testLabel = df3.as_matrix(columns=["Survived"]).astype(float)
